828cf01a77e23ebdaa3cf82aa1c8223ec98b3517
mgymrek <mgymrek@mit.edu>
2014-03-13 17:55:03 -0400

    adding test files so make distcheck works

--- tests/command_line_tests.sh
+++ tests/command_line_tests.sh
@@ -8,86 +8,87 @@ OUTDIR=$(mktemp -d) || exit 1
 testcode() {
   if [ $? != "$@" ]; then exit 1; fi;
 }
+
 ## Show Environment
 
 echo "### lobSTR tests ###"
 echo "Testing bam input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f test.aligned.sorted.bam \
+  -f ${LOBSTR_TEST_DIR=.}/test.aligned.sorted.bam \
   --rg-lib test --rg-sample test \
-  --bam >/dev/null 2>&1
+  --bam  >/dev/null 2>&1
 testcode 0 
 echo "Testing fastq input single end..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f tmp_1.fq \
+  -f ${LOBSTR_TEST_DIR=.}/tmp_1.fq \
   -q \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing fastq input paired end..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --p1 tmp_1.fq --p2 tmp_2.fq \
+  --p1 ${LOBSTR_TEST_DIR=.}/tmp_1.fq --p2 ${LOBSTR_TEST_DIR=.}/tmp_2.fq \
   -q \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing bam paired end input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f test.nosample.bam \
+  -f ${LOBSTR_TEST_DIR=.}/test.nosample.bam \
   --bampair \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing zipped fastq input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f test.fq.gz \
+  -f ${LOBSTR_TEST_DIR=.}/test.fq.gz \
   -q \
   --gzip \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing fasta single end input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f tmp_1.fa \
+  -f ${LOBSTR_TEST_DIR=.}/tmp_1.fa \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing fasta paired end input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --p1 tmp_1.fa --p2 tmp_2.fa \
+  --p1 ${LOBSTR_TEST_DIR=.}/tmp_1.fa --p2 ${LOBSTR_TEST_DIR=.}/tmp_2.fa \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing gzipped fasta input..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f test.fa.gz \
+  -f ${LOBSTR_TEST_DIR=.}/test.fa.gz \
   --gzip \
   --rg-lib test --rg-sample test >/dev/null 2>&1
 testcode 0
 echo "Testing fastq input single end with multithread..."
 lobSTR \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  -f tmp_1.fq \
+  -f ${LOBSTR_TEST_DIR=.}/tmp_1.fq \
   -p 2 \
   -q \
   --rg-lib test --rg-sample test >/dev/null 2>&1
@@ -97,52 +98,52 @@ echo
 echo "Testing good bam input..."
 allelotype \
   --command classify \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
-  --strinfo smallref/smallref_strinfo.tab \
-  --bam test.aligned.sorted.bam,test.aligned.sorted.bam \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
+  --strinfo ${LOBSTR_TEST_DIR=.}/smallref/smallref_strinfo.tab \
+  --bam ${LOBSTR_TEST_DIR=.}/test.aligned.sorted.bam,${LOBSTR_TEST_DIR=.}/test.aligned.sorted.bam \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --noise_model ../models/illumina_v2.0.3 >/dev/null 2>&1
+  --noise_model ${LOBSTR_TEST_DIR=.}/../models/illumina_v2.0.3 >/dev/null 2>&1
 testcode 0
 echo "Testing invalid path to bam input..."
 allelotype \
   --command classify \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
-  --strinfo smallref/smallref_strinfo.tab \
-  --bam bad.bam \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
+  --strinfo ${LOBSTR_TEST_DIR=.}/smallref/smallref_strinfo.tab \
+  --bam ${LOBSTR_TEST_DIR=.}/bad.bam \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --noise_model ../models/illumina_v2.0.3 >/dev/null 2>&1
+  --noise_model ${LOBSTR_TEST_DIR=.}/../models/illumina_v2.0.3 >/dev/null 2>&1
 testcode 1
 echo "Testing bam file with no index..."
 allelotype \
   --command classify \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
-  --strinfo smallref/smallref_strinfo.tab \
-  --bam test.noindex.bam \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
+  --strinfo ${LOBSTR_TEST_DIR=.}/smallref/smallref_strinfo.tab \
+  --bam ${LOBSTR_TEST_DIR=.}/test.noindex.bam \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --noise_model ../models/illumina_v2.0.3 >/dev/null 2>&1
+  --noise_model ${LOBSTR_TEST_DIR=.}/../models/illumina_v2.0.3 >/dev/null 2>&1
 testcode 1
 echo "Testing bam file with no sample in read group..."
 allelotype \
   --command classify \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
-  --strinfo smallref/smallref_strinfo.tab \
-  --bam test.nosample.bam \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
+  --strinfo ${LOBSTR_TEST_DIR=.}/smallref/smallref_strinfo.tab \
+  --bam ${LOBSTR_TEST_DIR=.}/test.nosample.bam \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --noise_model ../models/illumina_v2.0.3 >/dev/null 2>&1
+  --noise_model ${LOBSTR_TEST_DIR=.}/../models/illumina_v2.0.3 >/dev/null 2>&1
 testcode 0
 echo "Testing bam file with no read group..."
 allelotype \
   --command classify \
-  --index-prefix smallref/small_lobstr_ref/lobSTR_ \
-  --strinfo smallref/smallref_strinfo.tab \
-  --bam test.norg.bam \
+  --index-prefix ${LOBSTR_TEST_DIR=.}/smallref/small_lobstr_ref/lobSTR_ \
+  --strinfo ${LOBSTR_TEST_DIR=.}/smallref/smallref_strinfo.tab \
+  --bam ${LOBSTR_TEST_DIR=.}/test.norg.bam \
   --out ${OUTDIR}/lobtest \
   --verbose \
-  --noise_model ../models/illumina_v2.0.3 >/dev/null 2>&1
+  --noise_model ${LOBSTR_TEST_DIR=.}/../models/illumina_v2.0.3 >/dev/null 2>&1
 testcode 1
 
 exit 0
