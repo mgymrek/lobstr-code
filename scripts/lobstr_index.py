26892c30a73ec91031e67cf162603276480f199d
mgymrek <mgymrek@mit.edu>
2014-03-13 21:38:28 -0400

    redoing indexing script to make only one index

--- scripts/lobstr_index.py
+++ scripts/lobstr_index.py
@@ -13,7 +13,10 @@ Table mapping reference regions to which STRs they contain
 import argparse
 import os
 import pyfasta
+import shutil
+from subprocess import Popen, PIPE, STDOUT
 import sys
+import tempfile
 
 STRREFFILE = None
 REFFASTA = None
@@ -21,6 +24,7 @@ OUTDIR = None
 EXTEND = 1000
 VERBOSE = False
 PAD = 50
+DEBUG = False
 
 ###########################
 # methods
@@ -28,6 +32,25 @@ nucToNumber={"A":0,"C":1,"G":2,"T":3}
 
 def PROGRESS(msg):
     sys.stderr.write(msg.strip() + "\n")
+
+def RunCommand(cmd):
+    if DEBUG:
+        PROGRESS("CMD: %s"%cmd)
+        return
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, \
+                  stderr=STDOUT, close_fds=True)
+    print p.stdout.read()
+    ex = p.wait()
+    if ex != 0:
+        PROGRESS("ERROR: command '%s' failed"%cmd)
+        sys.exit(1)
+
+def GetRepseq(repseq):
+    """ Get canonical STR sequence, considering both strands """
+    repseq_f = getCanonicalMS(repseq)
+    repseq_r = getCanonicalMS(reverseComplement(repseq))
+    repseq = compareString(repseq_f, repseq_r)
+    return repseq
 
 def getCanonicalMS(repseq):
     """ Get canonical STR sequence """
@@ -65,25 +88,64 @@ def reverseComplement(seq):
     return newseq
 
 def WriteChromSizes(genome, outfile):
-    f = open(outfilel, "w")
+    f = open(outfile, "w")
     for chrom in genome.keys():
-        f.write("\t".join([chrom, len(genome[chrom])])+"\n")
+        f.write("\t".join(map(str,[chrom, len(genome[chrom])]))+"\n")
     f.close()
 
 def PadFlank(seq, n):
     return "N"*n+seq+"N"*n
 
-def GenerateMergedReferenceBed(str_ref_file, merged_str_file):
+def GenerateMergedReferenceBed(str_ref_file, annotated_ref):
     """ Generated merged reference region """
-    # TODO
+    # Get temp file names
+    tmpdir = tempfile.mkdtemp()
+    sorted_ref_withflank = os.path.join(tmpdir, "lobSTR_ref_plusflank_sorted.bed")
+    merged_ref = os.path.join(tmpdir, "lobSTR_ref_merged.bed")
+    # Generate reference file
+    cmd_sort = """cat %s | awk '{print $1 "\\t" $2-1000 "\\t" $3+1000 "\\t" $0}' | cut -f 4 --complement | \
+awk '($2>0)' | cut -f 1-5,17 | sortBed -i stdin > %s """%(str_ref_file, sorted_ref_withflank)
+    RunCommand(cmd_sort)
+    cmd_merge = """mergeBed -i %s > %s"""%(sorted_ref_withflank, merged_ref)
+    RunCommand(cmd_merge)
+    cmd_annot = """intersectBed -a %s -b %s -wa -wb | awk '{print $1 "\\t" $2 "\\t" $3 "\\t" $7"_"$8"_"$9";"}' | \
+bedtools groupby -g 1,2,3 -c 4 -o concat > %s"""%(merged_ref, sorted_ref_withflank, annotated_ref)
+    RunCommand(cmd_annot)
+    # Clean up
+    shutil.rmtree(tmpdir)
 
 def GetRefFasta(merged_str_file, str_ref_fasta, str_map_file):
     """ Get reference fasta and map file from the bed file """
-    # TODO
+    f_merged = open(merged_str_file, "r")
+    f_fa = open(str_ref_fasta, "w")
+    f_map = open(str_map_file, "w")
+    # Write fasta and map entry for each reference chunk
+    line = f_merged.readline()
+    refnum = 0
+    while line != "":
+        chrom, start, end, annot = line.strip().split("\t")
+        start = int(start)
+        end = int(end)
+        if end >= len(genome[chrom]): end = len(genome[chrom])-1
+        refseq = PadFlank(genome[chrom][start:end], PAD)
+        annotations = annot.split(";")[:-1]
+        motifs = [GetRepseq(item.split("_")[2]) for item in annotations]
+        annotations = [annotations[i] + "_" + motifs[i] for i in range(len(annotations))]
+        # Write to fasta file
+        f_fa.write(">%s$%s$%s$%s\n"%(refnum, chrom, start, end))
+        f_fa.write("%s\n"%refseq)
+        # Write to map file
+        f_map.write("\t".join(map(str,[refnum, ";".join(set(motifs)), ";".join(annotations)]))+"\n")
+        # Get next record
+        line = f_merged.readline()
+        refnum += 1
+    f_merged.close()
+    f_fa.close()
+    f_map.close()
 
 def bwaIndex(str_ref_fasta):
-    cmd = "lobSTRIndex index -a is %s"str_ref_fasta
-    os.system(cmd)
+    cmd = "lobSTRIndex index -a is %s"%str_ref_fasta
+    RunCommand(cmd)
 
 ##########################
 if __name__ == "__main__":
@@ -93,23 +155,26 @@ if __name__ == "__main__":
     parser.add_argument("--out_dir", help="Path to write results to", required=True, type=str)
     parser.add_argument("--extend", help="Length of flanking region to include on either side of the STR. (default 1000)", required=False, type=int)
     parser.add_argument("--verbose", help="Print out useful messages", required=False, action="store_true")
+    parser.add_argument("--debug", help="Don't run commands, just pring them", required=False, action="store_true")
 
     args = parser.parse_args()
     STRREFFILE = args.str
     REFFILE = args.ref
     OUTDIR = args.out_dir
+    if not os.path.exists(OUTDIR): os.mkdir(OUTDIR)
     if args.verbose: VERBOSE = True
+    if args.debug: DEBUG = True
 
-    if VERBOSE: PROGRESS("Loading gnome...")
+    if VERBOSE: PROGRESS("Loading genome...")
     genome = pyfasta.Fasta(REFFILE)
-    WriteChromSizes(genome, os.path.join(OUTDIR, "lobSTR_chromsizes.tab"))
+    if not DEBUG: WriteChromSizes(genome, os.path.join(OUTDIR, "lobSTR_chromsizes.tab"))
 
     if VERBOSE: PROGRESS("Processing STR table...")
     merged_str_file = os.path.join(OUTDIR, "lobSTR_mergedref.bed")
     str_ref_fasta = os.path.join(OUTDIR, "lobSTR_ref.fasta")
     str_map_file = os.path.join(OUTDIR, "lobSTR_ref_map.tab")
     GenerateMergedReferenceBed(STRREFFILE, merged_str_file)
-    GetRefFasta(merged_str_file, str_ref_fasta, str_map_file)
+    if not DEBUG: GetRefFasta(merged_str_file, str_ref_fasta, str_map_file)
 
     if VERBOSE: PROGRESS("Building BWA index...")
     bwaIndex(str_ref_fasta)
